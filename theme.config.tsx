c08b1b9a13354eeedeca0834abfe0aec49b45889
Nicolas AMATI <skkyz3r.dev@gmail.com>
2023-12-25 11:31:31 +0100

    feat: :tada: Init Nextra project

---
+++ theme.config.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default {
+  logo: <span>CMS EVENT WIKI</span>,
+  project: {
+    link: "https://github.com/TallIceCreamCake/CMS-EVENT-WIKI",
+  },
+  // ... other theme options
+};
